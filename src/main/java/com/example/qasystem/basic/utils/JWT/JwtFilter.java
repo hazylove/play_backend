9df0db84da9eda7ec9927d121c73bff792291c1e
BuJing <bojing@f2s2c.com>
2024-04-08 16:49:32 +0800

    pref:修改配置为本地，token解析错误处理

--- src/main/java/com/example/qasystem/basic/utils/JWT/JwtFilter.java
+++ src/main/java/com/example/qasystem/basic/utils/JWT/JwtFilter.java
@@ -1,6 +1,7 @@
 package com.example.qasystem.basic.utils.JWT;
 
 import com.alibaba.fastjson.JSONObject;
+import io.jsonwebtoken.JwtException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -58,11 +59,16 @@ public class JwtFilter extends OncePerRequestFilter {
 
         if (StringUtils.hasText(authToken) && authToken.startsWith("Bearer ")) {
             authToken = authToken.substring(7);
-
-            String userId = jwtUtil.getUserIdFromToken(authToken);
+            String userId;
+            try {
+                userId = jwtUtil.getUserIdFromToken(authToken);
+            } catch (JwtException ignored) {
+                sendUnauthorizedResponse(httpServletResponse, HttpStatus.UNAUTHORIZED.value(), "token解析失败，请携带正确token！");
+                return;
+            }
             Object tokenInRedis = redisTemplate.opsForValue().get("TOKEN_" + userId);
             if (tokenInRedis == null || !tokenInRedis.equals(authToken)) {
-                sendUnauthorizedResponse(httpServletResponse,  HttpStatus.UNAUTHORIZED.value(), "Token验证失败或已过期，请重新登录！");
+                sendUnauthorizedResponse(httpServletResponse, HttpStatus.UNAUTHORIZED.value(), "Token验证失败或已过期，请重新登录！");
                 return;
             }
 
@@ -70,7 +76,7 @@ public class JwtFilter extends OncePerRequestFilter {
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userId, null, null);
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
 
-                redisTemplate.opsForValue().set(userId, authToken, jwtUtil.getExpiration(), TimeUnit.SECONDS);
+                redisTemplate.opsForValue().set("TOKEN_" + userId, authToken, jwtUtil.getExpiration(), TimeUnit.SECONDS);
             }
         }
 
