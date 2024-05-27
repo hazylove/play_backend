c5369cf2c294bd54156440d90d5358009bec98b2
BuJing <bojing@f2s2c.com>
2024-05-27 16:31:47 +0800

    feat:处理本地资源映射地址

--- src/main/java/com/play/playsystem/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/play/playsystem/user/service/impl/UserServiceImpl.java
@@ -11,7 +11,6 @@ import com.play.playsystem.basic.utils.tool.MyFileUtil;
 import com.play.playsystem.basic.utils.tool.RedisUtil;
 import com.play.playsystem.basic.utils.result.JsonResult;
 import com.play.playsystem.basic.utils.result.ResultCode;
-import com.play.playsystem.basic.utils.tool.SysUtil;
 import com.play.playsystem.file.domain.entity.UploadFile;
 import com.play.playsystem.file.service.IUploadFileService;
 import com.play.playsystem.user.service.IUserService;
@@ -219,7 +218,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
                 .eq(User::getId, userId);
         User user = userMapper.selectOne(queryWrapper);
         // 重新设置用户头像地址
-        user.setAvatar(reBuildAvatarUrl(user.getAvatar()));
+        user.setAvatar(MyFileUtil.reSetFileUrl(user.getAvatar()));
         return user;
     }
 
@@ -234,9 +233,5 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
                 )
                 .eq(User::getId, userId);
         return userMapper.selectOne(queryWrapper);
-    }
-
-    private String reBuildAvatarUrl(String avatarUrl) {
-        return "http://" + SysUtil.getServerIpAddress() + ":" + SysUtil.port + SysUtil.patternPath + "/" + avatarUrl;
     }
 }
